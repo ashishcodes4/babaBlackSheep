ed98c14a3bc865bd41d40a400703513de126794d
ashishcodes4 <ashishcodes4@gmail.com>
2018-02-24 10:08:54 +0530

    fixed small bugs for pageNavigation..

--- src/Components/BodyMidSec/Pagenavigation.js
+++ src/Components/BodyMidSec/Pagenavigation.js
@@ -14,17 +14,13 @@ const Pagenavigation = ({ onRouteChange }) => {
 	return (
 		<div className='page-navigation'>
 			<div className='text-banner banner-menu'>
-				<a href='menu' onClick={()=> onRouteChange('menu')} >menu</a>
+				<a onClick={()=> onRouteChange('menu')} >menu</a>
 			</div>
 			<div className='text-banner banner-about'>
-				<a href='story' onClick={()=> onRouteChange('about')}>story</a>
+				<a onClick={()=> onRouteChange('about')}>story</a>
 			</div>
 			<div className='text-banner banner-gallery'>
-<<<<<<< HEAD
-				<a href='gallery' onClick={()=> onRouteChange('gallery')}>gallery</a>
-=======
-				<a href='#' onClick={()=>onRouteChange('gallery')}>gallery</a>
->>>>>>> 9b7f2aa1344755c73ce02cce6de06261e82e6f87
+				<a onClick={()=> onRouteChange('gallery')}>gallery</a>
 			</div>
 		</div>
 		);
